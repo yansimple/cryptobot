b078ac0f33d8ac763469e944c51fc74defef3b18
yansimple <yandanil2017@yandex.ru>
2021-04-13 03:54:12 +0300

    FINAL

--- bot.py
+++ bot.py
@@ -3,14 +3,15 @@ import telebot
 from telebot import types
 
 bot = telebot.TeleBot(config.token)
-#yansimple
 
 
+# yansimple
 
 @bot.message_handler(commands=["start"])
 def repeat_all_messages(message):
     id = message.chat.id
     id = str(id)
+
     def add_DATABASE():
         db = open("DataBaseId.txt", "r")
         List_Id = db.read()
@@ -26,8 +27,8 @@ def repeat_all_messages(message):
             input()
         else:
             print("ID " + id + " allready in DataBase")
-    add_DATABASE()
 
+    add_DATABASE()
 
     keyboard = types.InlineKeyboardMarkup()
     callback_button1 = types.InlineKeyboardButton(text="Chcę nauczyć się handlować", callback_data="answ1")
@@ -39,6 +40,7 @@ def repeat_all_messages(message):
     keyboard.add(callback_button3)
     keyboard.add(callback_button4)
     bot.send_message(message.chat.id, "Dlaczego interesujesz się Bitcoinem?", reply_markup=keyboard)
+
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_inline(call):
@@ -62,19 +64,24 @@ def callback_inline(call):
         keyboard.add(callback_button2)
         keyboard.add(callback_button3)
         keyboard.add(callback_button4)
-        bot.send_message(call.message.chat.id, "Jaki poziom dochodów jest Twoim ostatecznym celem?", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Jaki poziom dochodów jest Twoim ostatecznym celem?",
+                         reply_markup=keyboard)
 
     if call.data == "answ3":
         keyboard = types.InlineKeyboardMarkup()
         callback_button1 = types.InlineKeyboardButton(text="Jestem osobą spontaniczną", callback_data="answ4")
-        callback_button2 = types.InlineKeyboardButton(text="Gromadząc i analizując jak najwięcej informacji", callback_data="answ4")
-        callback_button3 = types.InlineKeyboardButton(text="Konsultując się z ekspertami / profesjonalistami", callback_data="answ4")
-        callback_button4 = types.InlineKeyboardButton(text="Konsultując się z rodziną lub przyjaciółmi", callback_data="answ4")
+        callback_button2 = types.InlineKeyboardButton(text="Gromadząc i analizując jak najwięcej informacji",
+                                                      callback_data="answ4")
+        callback_button3 = types.InlineKeyboardButton(text="Konsultując się z ekspertami / profesjonalistami",
+                                                      callback_data="answ4")
+        callback_button4 = types.InlineKeyboardButton(text="Konsultując się z rodziną lub przyjaciółmi",
+                                                      callback_data="answ4")
         keyboard.add(callback_button1)
         keyboard.add(callback_button2)
         keyboard.add(callback_button3)
         keyboard.add(callback_button4)
-        bot.send_message(call.message.chat.id, "Jak podchodzisz do podejmowania strategicznych decyzji?", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Jak podchodzisz do podejmowania strategicznych decyzji?",
+                         reply_markup=keyboard)
 
     if call.data == "answ4":
         keyboard = types.InlineKeyboardMarkup()
@@ -88,14 +95,18 @@ def callback_inline(call):
         keyboard.add(callback_button3)
         keyboard.add(callback_button4)
         keyboard.add(callback_button5)
-        bot.send_message(call.message.chat.id, "Sądząc z własnego doświadczenia, jak skuteczne były Twoje inwestycje w tych dziedzinach?", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id,
+                         "Sądząc z własnego doświadczenia, jak skuteczne były Twoje inwestycje w tych dziedzinach?",
+                         reply_markup=keyboard)
 
     if call.data == "answ5":
         keyboard = types.InlineKeyboardMarkup()
         callback_button1 = types.InlineKeyboardButton(text="Kupiłbym drogi samochód", callback_data="answ6")
         callback_button2 = types.InlineKeyboardButton(text="Kupiłbym ładny dom", callback_data="answ6")
-        callback_button3 = types.InlineKeyboardButton(text="Kupiłbym udziały w odnoszącej sukcesy firmie", callback_data="answ6")
-        callback_button4 = types.InlineKeyboardButton(text="Spędziłbym to wszystko z przyjaciółmi", callback_data="answ6")
+        callback_button3 = types.InlineKeyboardButton(text="Kupiłbym udziały w odnoszącej sukcesy firmie",
+                                                      callback_data="answ6")
+        callback_button4 = types.InlineKeyboardButton(text="Spędziłbym to wszystko z przyjaciółmi",
+                                                      callback_data="answ6")
         callback_button5 = types.InlineKeyboardButton(text="Inwestowałbym na rynku walutowym", callback_data="answ6")
         keyboard.add(callback_button1)
         keyboard.add(callback_button2)
@@ -114,15 +125,20 @@ def callback_inline(call):
         keyboard.add(callback_button2)
         keyboard.add(callback_button3)
         keyboard.add(callback_button4)
-        bot.send_message(call.message.chat.id, "Ile masz kapitału na swoją początkową inwestycję?", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id, "Ile masz kapitału na swoją początkową inwestycję?",
+                         reply_markup=keyboard)
 
     if call.data == "answ7":
         keyboard = types.InlineKeyboardMarkup()
-        url_button = types.InlineKeyboardButton(text="Twój wiek", url="https://dijaceqa.momonito.com/tracker?offer_id=3836&aff_id=9756")
+        url_button = types.InlineKeyboardButton(text="Twój wiek",
+                                                url="https://dijaceqa.momonito.com/tracker?offer_id=3836&aff_id=9756")
 
         keyboard.add(url_button)
 
-        bot.send_message(call.message.chat.id, "Proszę wprowadzić swój wiek. Aby wziąć udział, musisz mieć ukończone 18 lat", reply_markup=keyboard)
+        bot.send_message(call.message.chat.id,
+                         "Proszę wprowadzić swój wiek. Aby wziąć udział, musisz mieć ukończone 18 lat",
+                         reply_markup=keyboard)
+
 
 if __name__ == '__main__':
     bot.infinity_polling()
